9cd0b9930f83e6d13bde180490a9141650592bff
Akanksha Singh <akankshasingh121297@gmail.com>
2019-10-20 13:40:02 +0530

    Update problem 3 in c++

--- problem_3/problem_3.cpp
+++ problem_3/problem_3.cpp
@@ -1,1 +1,12 @@
-//Wirte your code logic here!
+//Wirte your code logic here
+int birthdayCakeCandles(vector<int> ar) {
+sort(ar.begin(),ar.end());
+int count=0;
+int s=ar.size();
+for(int i=s;i>0;i--)
+{
+    if(ar[s-1]==ar[i-1])
+    count++;
+}
+return count;
+}
