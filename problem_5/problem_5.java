165af26d9d6d3fd616c46c0cbeb4ce19123c8127
anshcena <anshcena.1996@gmail.com>
2019-10-20 16:56:08 +0530

    (ADDED) Question 5 Grading

--- problem_5/problem_5.java
+++ problem_5/problem_5.java
@@ -1,1 +1,27 @@
-//Wirte your code logic here!
+int size= grades.size();
+    int t = 0,check =0;
+    List<Integer> newArr = new ArrayList<Integer>(size);
+    for(int i = 0;  i < size;  i++) {
+        if(grades.get(i) < 40 ) {
+            if(grades.get(i) >= 38){
+                newArr.add(40);
+            }else{
+            newArr.add(grades.get(i));
+            }
+        }
+        else if(grades.get(i)% 5 == 0 && grades.get(i) >= 40){
+            newArr.add(grades.get(i));
+        }
+        else{
+            t = grades.get(i)/5 + 1;
+            t = t * 5;
+            check = t - grades.get(i);
+            if (check < 3) {
+                newArr.add(t);
+            } else  {
+                newArr.add(grades.get(i));
+            }
+        }
+    }
+    return newArr;
+    }
