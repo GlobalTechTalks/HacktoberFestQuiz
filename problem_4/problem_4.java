96ca123bc90f56ed06e7e9f957b8092a86378a1c
anshcena <anshcena.1996@gmail.com>
2019-10-20 14:49:49 +0530

    (Updated) Problem 4 not 14 Tiime conversion

--- problem_4/problem_4.java
+++ problem_4/problem_4.java
@@ -1,1 +1,15 @@
 //Wirte your code logic here!
+
+ static String timeConversion(String s) {
+        int r,num;
+        String ss="";
+        String s1 ="";
+        int i = 0;
+    r=s.indexOf(':');
+    ss = s.substring(0,r);
+     num = Integer.parseInt(ss);
+    num = num + 12;
+    s1= "" + num + s.substring(r,s.length()-2);
+       return s1;
+
+    }
