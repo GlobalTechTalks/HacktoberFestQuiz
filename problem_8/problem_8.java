ff89bfce6477c24a6ed78f3e848fd639dd064d09
AndroidAssister <swadeepsonijpr0141@gmail.com>
2019-10-20 15:02:33 +0530

    swadeep pro-8

--- problem_8/problem_8.java
+++ problem_8/problem_8.java
@@ -1,1 +1,46 @@
-//Wirte your code logic here!
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    static int[] getRecord(int[] s){
+        // Complete this function
+        int[] result = new int[2];
+
+        int max  = s[0];
+        int min = s[0];
+
+        for (int i=1; i < s.length ; i++){
+            if(s[i]>max){
+                result[0]++;
+                max = s[i];
+            }
+
+            if(s[i]<min){
+                result[1]++;
+                min = s[i];
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        int[] s = new int[n];
+        for(int s_i=0; s_i < n; s_i++){
+            s[s_i] = in.nextInt();
+        }
+        int[] result = getRecord(s);
+        String separator = "", delimiter = " ";
+        for (Integer val : result) {
+            System.out.print(separator + val);
+            separator = delimiter;
+        }
+        System.out.println("");
+    }
+}
