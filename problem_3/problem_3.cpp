9bc11f2381902548c7c1035ea913000b68907b0f
PIYUSH KHURANA <52041719+PIYUSH-01@users.noreply.github.com>
2019-10-20 14:53:51 +0530

    Update problem_3.cpp

--- problem_3/problem_3.cpp
+++ problem_3/problem_3.cpp
@@ -1,1 +1,12 @@
-//Wirte your code logic here!
+int birthdayCakeCandles(vector<int> ar) {
+    int n = sizeof(ar)/sizeof(ar[0]); 
+    sort(ar.begin(), ar.end(), greater<int> ());
+    
+    int i = 0;
+    
+    while(ar[i] == ar[i+1]) {
+        i++;
+    }
+
+    return i+1;
+}
