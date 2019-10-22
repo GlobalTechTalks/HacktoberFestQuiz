f33c4fa3a28a49865ad4feace5d8a0bac4161ce0
anshcena <anshcena.1996@gmail.com>
2019-10-22 15:17:56 +0530

    (UPDATED PROBLEM 4) Time conversion updated with all passed test cases

--- problem_4/problem_4.java
+++ problem_4/problem_4.java
@@ -1,15 +1,23 @@
-//Wirte your code logic here!
+static String timeConversion(String s) {
+        int num;    
+        String res ="";  // store final time value
+        String temp = "0"; // add 0 before single digit time
+        int r = s.indexOf(':');     // storing index of first ':''
+        
+        // type conversion
+        num = Integer.parseInt(s.substring(0,r));
+        if((s.substring(s.length()-2,s.length()).equals("PM"))){
+                temp = "";
+                num = num + 12;
+            if(num == 24) {
+                num = num - 12;
+                 }       
+    }
+     else {
+        if(num == 12) 
+            num = 0;         
+    } 
 
- static String timeConversion(String s) {
-        int r,num;
-        String ss="";
-        String s1 ="";
-        int i = 0;
-    r=s.indexOf(':');
-    ss = s.substring(0,r);
-     num = Integer.parseInt(ss);
-    num = num + 12;
-    s1= "" + num + s.substring(r,s.length()-2);
-       return s1;
-
+    res = temp + num + s.substring(r,s.length()-2);
+       return res;
     }
